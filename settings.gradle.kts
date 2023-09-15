7dff8f4f86142384bc45ce890c1a9324e2d250f3
CaptainIsHere <stevekgeorge123@gmail.com>
2023-09-15 12:51:50 -0400

    Created project

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Team 7 Project"
+include(":app")
