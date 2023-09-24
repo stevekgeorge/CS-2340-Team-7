60280613935a2511ce251973141caa58a08d8105
gwetherby <gabrielwetherby@gmail.com>
2023-09-24 19:46:02 -0400

    Functionality for screens implemented

--- app/src/main/java/com/example/team7project/IntroScreen.java
+++ app/src/main/java/com/example/team7project/IntroScreen.java
@@ -1,14 +1,54 @@
 package com.example.team7project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class IntroScreen extends AppCompatActivity {
+
+    private EditText name;
+    private RadioGroup difficulty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_screen);
+
+        name = findViewById(R.id.nameInput);
+        difficulty = findViewById(R.id.difficultyRadioGroup);
+        Button continueButton = findViewById(R.id.startButton7);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get user input
+                String playerName = name.getText().toString();
+                int selectedDifficultyId = difficulty.getCheckedRadioButtonId();
+
+                // Check if the name and difficulty are entered
+                if (playerName.isEmpty() || selectedDifficultyId == -1) {
+                    Toast.makeText(IntroScreen.this, "Please fill out all fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Retrieve selected difficulty
+                    RadioButton selectedRadioButton = findViewById(selectedDifficultyId);
+                    String selectedDifficulty = selectedRadioButton.getText().toString();
+
+                    // Handle the data (e.g., pass it to the next activity)
+                    // For simplicity, we'll just show a Toast message here
+                    String message = "Player Name: " + playerName + "\nDifficulty: " + selectedDifficulty;
+                    Toast.makeText(IntroScreen.this, message, Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(IntroScreen.this, PlayerSelectScreen.class);
+                    startActivity(intent);
+                    // TODO: Add code to navigate to the next screen (e.g., PlayerSelectActivity)
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/team7project/PlayerSelectScreen.java
+++ app/src/main/java/com/example/team7project/PlayerSelectScreen.java
@@ -1,8 +1,10 @@
 package com.example.team7project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class PlayerSelectScreen extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class PlayerSelectScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_select_screen);
+
+        ImageButton character1Button = findViewById(R.id.character1Button);
+        ImageButton character2Button = findViewById(R.id.character2Button);
+        ImageButton character3Button = findViewById(R.id.character3Button);
+
+        character1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the next activity
+                Intent intent = new Intent(PlayerSelectScreen.this, PlayerSelectScreen.class);
+
+                // Add any necessary data as extras to the intent
+                intent.putExtra("selectedCharacter", "Sword Master Sid");
+
+                // Start the next activity
+                startActivity(intent);
+            }
+        });
     }
 }
