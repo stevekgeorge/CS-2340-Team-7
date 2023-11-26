4b4cc9e08e7fa66b66c1034453d37cbd9e3b79bb
anorouzinia <ava.norouzinia@gmail.com>
2023-11-26 12:21:20 -0500

    documentation for the three player sprites

--- android/src/com/cs2340team7/project/models/SwordMasterSid.java
+++ android/src/com/cs2340team7/project/models/SwordMasterSid.java
@@ -15,6 +15,14 @@ public class SwordMasterSid implements MovementStrategy {
     SwordMasterSid(Player player) {
         this.player = player;
     }
+    /**
+     * method that moves sprite based on button that is clicked.
+     * checks to see if sprite is moving off the screen or not.
+     * Updates every subscriber in the PlayerPositionSubscriber arraylist of the sprites new location.
+     *  Implementation of the movementStrategy interface is part of the strategy design pattern
+     * and updating subscribers is part of the observer design pattern.
+     * @param direction
+     */
     @Override
     public void move(Player.Direction direction) {
 
