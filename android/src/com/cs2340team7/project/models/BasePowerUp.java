402ff3493548af5d1bd119c7871865e15baec939
gwetherby <gabrielwetherby@gmail.com>
2023-11-27 15:27:48 -0500

    Fixed merge issues and added this 1st phase of powerups.

---
+++ android/src/com/cs2340team7/project/models/BasePowerUp.java
@@ -0,0 +1,12 @@
+package com.cs2340team7.project.models;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+
+public abstract class BasePowerUp implements PowerUps {
+    private FileHandle fileHandle = Gdx.files.internal("KlausWall.png");
+    private Texture replacementTile = new Texture(fileHandle);
+    @Override
+    public abstract void apply(GameDataModel model);
+}
