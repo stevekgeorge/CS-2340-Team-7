965cf69168664a5a1bba1eb804b18efa24b041be
anorouzinia <ava.norouzinia@gmail.com>
2023-11-27 20:48:03 -0500

    documentation TechGreenViewModel

--- android/src/com/cs2340team7/project/viewmodels/TechGreenViewModel.java
+++ android/src/com/cs2340team7/project/viewmodels/TechGreenViewModel.java
@@ -6,16 +6,27 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.cs2340team7.project.models.GameDataModel;
 import com.cs2340team7.project.models.Player;
+/**
+ * The TechGreenViewModel class serves as a ViewModel in the MVVM architecture for controlling the game's logic and data.
+ * It manages interactions between the game data, player, and graphical representations of the player's character.
+ */
 
 public class TechGreenViewModel extends ViewModel {
     private GameDataModel gameData;
     private Player player;
+    /**
+     * Constructs a new instance of TechGreenViewModel.
+     * Initializes the game data and player, and sets the player's movement strategy.
+     */
     public TechGreenViewModel() {
         gameData = GameDataModel.getData();
         player = Player.getPlayer();
         //I think there is a better way to couple movement strategy and player but not sure
         player.setMovementStrategy();
     }
+    /**
+     * Advances the current game level to the next level.
+     */
     public void advanceLevel() {
         gameData.setCurrentLevel(2);
     }
@@ -25,6 +36,11 @@ public class TechGreenViewModel extends ViewModel {
     public void updateMap(TiledMap map) {
         gameData.updateMap(map);
     }
+    /**
+     * Initiates a movement for the player in the specified direction.
+     *
+     * @param direction The direction in which the player should move.
+     */
     public void move(Player.Direction direction) {
         player.move(direction);
     }
@@ -34,6 +50,12 @@ public class TechGreenViewModel extends ViewModel {
     public float getY() {
         return player.getY();
     }
+    /**
+     * Updates the player's position to the specified coordinates.
+     *
+     * @param newX The new X-coordinate for the player.
+     * @param newY The new Y-coordinate for the player.
+     */
     public void updatePosition(int newX, int newY) {
         player.updatePosition(newX, newY);
     }
