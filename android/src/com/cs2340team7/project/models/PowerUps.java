366f7c6fc9c52c283cb151953a0731bb33bb3c87
anorouzinia <ava.norouzinia@gmail.com>
2023-11-27 19:58:20 -0500

    documentation for powerups

--- android/src/com/cs2340team7/project/models/PowerUps.java
+++ android/src/com/cs2340team7/project/models/PowerUps.java
@@ -1,5 +1,9 @@
 package com.cs2340team7.project.models;
 
+/**
+ * interface for PowerUps with an abstract method that applies powerups to the GameModel.
+ * This interface is part of the decorator pattern implementation.
+ */
 public interface PowerUps {
     void apply(GameDataModel model);
 }
