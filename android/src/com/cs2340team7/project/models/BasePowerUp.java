0e042a95d9a3889c1e2e8e3b8e98478b20d3e749
Steve K. George <stevekgeorge@lawn-10-91-10-188.lawn.gatech.edu>
2023-11-27 20:03:52 -0500

    Added BasePowerUp  Documentation

--- android/src/com/cs2340team7/project/models/BasePowerUp.java
+++ android/src/com/cs2340team7/project/models/BasePowerUp.java
@@ -1,3 +1,7 @@
+/**
+ * BasePowerUp is an abstract class that implements the PowerUps interface.
+ * It serves as the base class for different power-up types in the game.
+ */
 package com.cs2340team7.project.models;
 
 import com.badlogic.gdx.Gdx;
@@ -5,13 +9,32 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 
 public abstract class BasePowerUp implements PowerUps {
+    // File handle for the power-up texture
     private FileHandle fileHandle = Gdx.files.internal("KlausWall.png");
+
+    // Texture representing the power-up
     private Texture replacementTile = new Texture(fileHandle);
+
+    /**
+     * Applies the power-up effect to the game model.
+     * Subclasses must implement this method.
+     * @param model The game data model to which the power-up is applied.
+     */
     @Override
     public abstract void apply(GameDataModel model);
+
+    /**
+     * Gets the texture representing the power-up.
+     * @return The power-up texture.
+     */
     public Texture getTexture() {
         return replacementTile;
     }
+
+    /**
+     * Disposes of resources associated with the power-up.
+     * It is important to call this method when the power-up is no longer needed.
+     */
     public void dispose() {
         replacementTile.dispose();
     }
