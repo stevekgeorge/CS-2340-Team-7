4138fe589ad0eb74aabc82032730f69d22e8ce16
anorouzinia <ava.norouzinia@gmail.com>
2023-11-27 20:51:58 -0500

    SeniorEnemy

--- android/src/com/cs2340team7/project/models/SeniorEnemy.java
+++ android/src/com/cs2340team7/project/models/SeniorEnemy.java
@@ -4,9 +4,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-
+/**
+ * The SeniorEnemy class represents an enemy in the game that has a moderate level of difficulty.
+ * It extends the Enemy class and includes additional properties specific to senior-level enemies.
+ */
 public class SeniorEnemy extends Enemy {
     private int speed;
+    /**
+     * Constructs a new instance of the SeniorEnemy class with the specified initial position.
+     *
+     * @param x The initial X-coordinate of the senior enemy.
+     * @param y The initial Y-coordinate of the senior enemy.
+     */
     public SeniorEnemy(int x, int y) {
         super();
         FileHandle fileHandle = Gdx.files.internal("seniorenemy.png");
@@ -19,6 +28,12 @@ public class SeniorEnemy extends Enemy {
         this.getEnemySprite().setSize(this.getSizeX(), this.getSizeY());
         this.speed = 1;
     }
+    /**
+     * Moves the senior enemy in the specified direction.
+     * Overrides the move method in the superclass (Enemy).
+     *
+     * @param direction The direction in which the senior enemy should move.
+     */
     @Override
     public void move(Player.Direction direction) {
         //UpdatePlayer loop for the method for the enemy movements
