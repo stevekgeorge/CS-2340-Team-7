dfb4a4bd2f98e3b61c457d05a0ed5e40a74b1428
Steve K. George <stevekgeorge@lawn-10-91-10-188.lawn.gatech.edu>
2023-11-27 19:56:27 -0500

    Added PlayerSelectScreen Documentation

--- android/src/com/cs2340team7/project/views/PlayerSelectScreen.java
+++ android/src/com/cs2340team7/project/views/PlayerSelectScreen.java
@@ -1,3 +1,7 @@
+/**
+ * PlayerSelectScreen class represents the screen where the player selects a character before starting the game.
+ * It allows the player to choose from different characters and initiates the game with the selected character.
+ */
 package com.cs2340team7.project.views;
 
 import android.content.Intent;
@@ -20,19 +24,30 @@ public class PlayerSelectScreen extends AppCompatActivity {
     private PlayerSelectViewModel viewModel;
     private Intent beginIntent;
 
+    /**
+     * Overrides the onCreate method of AppCompatActivity to initialize the player selection screen.
+     * It sets up data binding, UI elements, and event listeners for character selection.
+     * @param savedInstanceState The saved instance state bundle.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set up data binding
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_player_select_screen);
         mainBinding.setLifecycleOwner(this);
         viewModel = new PlayerSelectViewModel();
         mainBinding.setViewModel(viewModel);
 
+        // Initialize UI elements
         ImageButton character1Button = findViewById(R.id.character1Button);
         ImageButton character2Button = findViewById(R.id.character2Button);
         ImageButton character3Button = findViewById(R.id.character3Button);
 
+        // Create an intent to start the game screen
         beginIntent = new Intent(PlayerSelectScreen.this, GameScreenLauncher.class);
+
+        // Set up click listeners for character buttons
         character1Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,10 +56,11 @@ public class PlayerSelectScreen extends AppCompatActivity {
                         DateFormat.getDateTimeInstance(
                                 DateFormat.SHORT,
                                 DateFormat.SHORT).format(
-                                        Calendar.getInstance().getTime()).toString());
+                                Calendar.getInstance().getTime()).toString());
                 startActivity(beginIntent);
             }
         });
+
         character2Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,11 +69,12 @@ public class PlayerSelectScreen extends AppCompatActivity {
                         DateFormat.getDateTimeInstance(
                                 DateFormat.SHORT,
                                 DateFormat.SHORT).format(
-                                        Calendar.getInstance().getTime()).toString());
+                                Calendar.getInstance().getTime()).toString());
 
                 startActivity(beginIntent);
             }
         });
+
         character3Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,7 +83,7 @@ public class PlayerSelectScreen extends AppCompatActivity {
                         DateFormat.getDateTimeInstance(
                                 DateFormat.SHORT,
                                 DateFormat.SHORT).format(
-                                        Calendar.getInstance().getTime()).toString());
+                                Calendar.getInstance().getTime()).toString());
                 startActivity(beginIntent);
             }
         });
